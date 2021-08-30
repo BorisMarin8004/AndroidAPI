4bb7f8a0cfdf6701b0b2a95ddeabb9a5f88e9add
BorisMarin8004 <bmar8004@gmail.com>
2021-08-29 21:03:24 -0700

    Added getUsersCall and relevant tests.

--- app/src/main/java/com/example/androidapi/MainActivity.java
+++ app/src/main/java/com/example/androidapi/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.androidapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView test;
+    private RetrofitClient retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        test = findViewById(R.id.test);
+        retrofit = RetrofitClient.getClient();
+
+        retrofit.getUsersCall().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if(response.isSuccessful()) {
+                    assert response.body() != null;
+                    test.setText(response.body().toString());
+                } else {
+                    Toast.makeText(getApplicationContext(), "response error: " + response.code(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                try {
+                    throw t;
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        });
+
+
+
     }
 }
--- app/src/test/java/com/example/androidapi/ExampleUnitTest.java
+++ app/src/test/java/com/example/androidapi/ExampleUnitTest.java
@@ -1,6 +1,17 @@
 package com.example.androidapi;
 
+import android.widget.Toast;
+
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +21,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private List<User> api_response_body;
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void getUsersCall_isCorrect() {
+        RetrofitClient retrofit = RetrofitClient.getClient();
+        retrofit.getUsersCall().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if(response.isSuccessful()) {
+                    assert response.body() != null;
+                    api_response_body = response.body();
+                    assertTrue(api_response_body.size() > 0);
+                } else {
+                    new Exception("Request failed, code: " + response.code()).printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                try {
+                    throw t;
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        });
     }
 }
